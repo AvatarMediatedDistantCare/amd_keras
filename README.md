16248c818c9d34bb577aa77d6117993ddb33eebe
Dai Hasegawa <daihasg@gmail.com>
2018-09-28 17:04:20 +0900

    add motion files and a script to create dataset

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # amd_keras
+
+### 1. データセット
+- data/csv/kinect/xxx.csv: 100次元(25関節x4次元数) x timestep
+- data/csv/mocap/xxx.csv: 100次元(25関節x4次元数) x timestep
+- data/csv/kinect_noised/xxx.csv: 100次元(25関節x4次元数) x timestep （ki_xxx に対してノイズを加えたもの）
+- data/data_path.csv: データセットを作成するデータを指定するcsvファイル(必要に応じて編集)
+
+- data/npy/: データセット保存先
+
+### 3. データセットの作成
+```sh
+cd data/
+python create_dataset.py 10 # 引数 10: num of context
+```
+
+### 4. モデルの作成
+1. `python train.py model.hdf5` ... `train.py` を動かす
+- (`train_***.py` に関しては、名前を変えて管理していただけなので処理に関しては特に違い等はありません。)
+
+『この段階での成果物』
+1. (train.pyと同じディレクトリ) / model.hdf5...学習モデル
+1. (train.pyと同じディレクトリ) / losses.png...学習における損失のグラフ
+
+### 5. フィルタのかけ方
+1. `node movingAverage_oneEuro.js (input file name) (window size)` ... 推定エラー補正後のモーションの移動平均フィルタとoneEuroフィルタを適用するために使用
+
+『この段階の成果物』
+1. (input file nameと同じディレクトリ) / OOO_apply_normalize.csv ... ノーマライズされたモーションデータ
+1. (input file nameと同じディレクトリ) / OOO_apply_one_euro_filter.csv ... one_euroフィルタが適用されたモーションデータ
+1. (input file nameと同じディレクトリ) / OOO_apply_moving_average_(window size).csv ... (window size)分の移動平均フィルタが適用されたモーションデータ
+
+### 6. 描画プログラム
+1. input_predicted_correct.js ... input, predicted, correctを比較するコード
+(比較したいもののためにコード内の `filename` 適宜変えてください)
+
+### 7. 定量的評価プログラムの使い方
+1. 以下のコードのfilename[0]~[2]の箇所を任意の値に変更
+  - https://github.com/banaoh/amd_kinect_quaternion/blob/master/js/quantitative_evaluation.js#L153-L157
+1. 以下のコメントアウトを消して、 `js/quantitative_evaluation.js` を読み込ませる
+  - https://github.com/banaoh/amd_kinect_quaternion/blob/master/index.html#L23
---
+++ data/create_dataset.py
@@ -0,0 +1,95 @@
+import sys
+import os
+import pandas as pd
+import numpy as np
+import math
+
+N_INPUT = 100 # Number of MFCC features
+N_OUTPUT = 100 # Number of gesture features
+DATA_FILE = ''
+N_CONTEXT = 10
+
+def create_vectors(kinect_filename, mocap_filename):
+    # Step 1:
+    # kinectの生データ読み込みー配列化, org[frame][quaternion = 100（25関節 x quaternion）]
+    # mocapの生データを読み込み -> 配列化, org[frame][quaternion = 100（25関節 x quaternion）]
+
+    f = open(kinect_filename, 'r')
+    kinect_data = f.readlines()
+    for idx, line in enumerate(kinect_data):
+        line = line.rstrip()
+        kinect_data[idx]= [float(x) if(x != '' and math.isnan(float(x)) != True) else 0.0 for x in line.split(',')]
+    input_vectors = np.array(kinect_data)
+
+    print(mocap_filename)
+    f = open(mocap_filename, 'r')
+    mocap_data = f.readlines()
+
+    # mocap[frame][quaternioni]の形にする.空白が入っている場合は0.0を代入
+    for idx, line in enumerate(mocap_data):
+        line = line.rstrip()
+        mocap_data[idx] = [float(x) if(x != '' and math.isnan(float(x)) != True) else 0.0  for x in line.split(',')]
+    output_vectors = np.array(mocap_data)
+
+    # Step 2: 短い方にあわせる
+    diff = len(input_vectors) - len(output_vectors)
+    if diff > 0:
+        input_vectors = input_vectors[0:len(input_vectors) - diff]
+    elif diff < 0:
+        output_vectors = output_vectors[0:len(output_vectors) + diff]
+
+    print('前後N_contextを見る前のshape')
+    print(input_vectors.shape)
+    print(output_vectors.shape)
+
+
+    # Step 3: N_CONTEXTずつとりだして格納して、１ずつSRIDEしていく
+    input_with_context = np.array([])
+    output_with_context = np.array([])
+
+    for i in range( len(input_vectors) - N_CONTEXT ):
+        if i == 0:
+            input_with_context = input_vectors[i:i + N_CONTEXT].reshape(1, N_CONTEXT, N_INPUT)
+            output_with_context = output_vectors[i + int(N_CONTEXT / 2)].reshape(1, N_OUTPUT)
+        else:
+            input_with_context = np.append(input_with_context,
+                                           input_vectors[i:i + N_CONTEXT].reshape(1, N_CONTEXT, N_INPUT), axis=0)
+            output_with_context = np.append(output_with_context, output_vectors[i + N_CONTEXT].reshape(1, N_OUTPUT),
+                                            axis=0)
+    print('前後N_contextを見たあとのshape')
+    print(input_with_context.shape)
+    print(input_with_context[1])
+    print(output_with_context.shape)
+    print(output_with_context[1])
+
+    return input_with_context, output_with_context
+    # return input_vectors, output_vectors
+
+# name = ('train' or 'dev') and 'test' 
+# kinectとmocapのそれぞれ対応するデータを元にnpyを作成
+def create(name):
+    DATA_FILE = pd.read_csv('data_path.csv')
+    X = np.array([])
+    Y = np.array([])
+    print(DATA_FILE['kinect_filename'][0])
+
+
+    for i in range(len(DATA_FILE)):
+        input_vectors, output_vectors = create_vectors(DATA_FILE['kinect_filename'][i], DATA_FILE['mocap_filename'][i])
+        if len(X) == 0:
+            X = input_vectors
+            Y = output_vectors
+        else:
+            X = np.concatenate((X, input_vectors), axis=0)
+            Y = np.concatenate((Y, output_vectors), axis=0)
+
+
+    x_file_name = 'npy/X.npy'
+    y_file_name = 'npy/Y.npy'
+    np.save(x_file_name, X)
+    np.save(y_file_name, Y)
+
+if __name__ == "__main__":
+    N_CONTEXT = int(sys.argv[1])
+    create('dev')
+    # create('test')
